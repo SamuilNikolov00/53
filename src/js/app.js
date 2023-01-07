02a6363741f8579e7bc264e1cdbaddbaa6bafbe4
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-07 21:27:41 +0200

    add: functionality

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,12 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+const products = document.getElementsByClassName("product");
+
+
+const price = document.getElementsByClassName("price")[0].textContent;
+for(let el of products){
+  el.setAttribute('data-price',price);
+}
 
 });
